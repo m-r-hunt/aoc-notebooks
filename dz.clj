f20dcf01cc1d867328208ae8580ec8c4a2eba100
Maximilian Hunt <max@huntw3.com>
2023-12-02 17:09:49 +0000

    Rename template to make replacing placeholder easier

---
+++ dz.clj
@@ -0,0 +1,13 @@
+;; # 2023 Day z
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns aoc-notebooks.solutions.2023.dz
+  (:require [aoc-notebooks.input :as input]))
+
+;; [Problem](https://adventofcode.com/2023/day/z)
+
+;; ## Part 1
+
+(def input (input/tokens 2023 z))
+
+;; ## Part 2
