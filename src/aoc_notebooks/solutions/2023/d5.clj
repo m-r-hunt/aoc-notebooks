b6a96929dc0b6286aa2a031240218aa1ca129585
Max Hunt <max@huntw3.com>
2023-12-05 17:21:37 +0000

    d5 done

--- src/aoc_notebooks/solutions/2023/d5.clj
+++ src/aoc_notebooks/solutions/2023/d5.clj
@@ -18,7 +18,7 @@
 
 (defn parse-map
   [[name] & ranges]
-  {:name name :ranges (mapv make-range ranges)})
+  {:name name :ranges (vec (sort-by :src (map make-range ranges)))})
 
 (apply parse-map (second input))
 
@@ -42,18 +42,35 @@
 
 (defn map-number-range
   [{:keys [start range-size] :as orig} ranges]
-  (if-let [{:keys [dest src size]} (first (filter (fn [{:keys [dest src size]}] (<= src start (+ src size -1))) ranges))]
-    (let [rest-size (- (+ src size) start)]
-      (if (>= rest-size range-size)
-        [{:start (+ dest (- start src)) :range-size range-size}]
-        (conj (map-number-range {:start (+ start rest-size) :range-size (- range-size rest-size)} ranges) {:start (+ dest (- start src)) :range-size rest-size}))) 
-    (if-let [new-start (first (filter #(not= % (map-number % ranges)) (range start (+ start range-size))))]
-      (conj (map-number-range {:start new-start :range-size (- range-size (- new-start start))}
-                              ranges)
-            {:start start :range-size (- new-start start)})
-      [orig])))
+  (let [ranges-start (get-in ranges [0 :src])]
+    (if (< start ranges-start)
+      (if (> (+ start range-size) ranges-start)
+        (conj (map-number-range {:start ranges-start :range-size (- range-size (- ranges-start start))} ranges) 
+              {:start start :range-size (- ranges-start start)})
+        [orig])
+      (let [[lower-ranges higher-ranges] (split-with #(<= (:src %) start) ranges)
+            last-range (last lower-ranges)
+            last-range-end (+ (:src last-range) (:size last-range))
+            next-range (first higher-ranges)
+            next-range-start (if next-range (:src next-range) Long/MAX_VALUE)
+            last-range-comp (if (< start last-range-end)
+                              [{:start (+ (:dest last-range) (- start (:src last-range))) :range-size (min range-size (- last-range-end start))}]
+                              [])
+            empty-space-comp (if (and (>= (+ start range-size) last-range-end) (> next-range-start last-range-end))
+                               (let [empty-space-end (min (+ start range-size) next-range-start)
+                                     empty-space-start (max start last-range-end)]
+                                 [{:start empty-space-start :range-size (- empty-space-end empty-space-start)}])
+                               [])
+            next-range-comp (if (and next-range (>= (+ start range-size) next-range-start))
+                              (map-number-range {:start next-range-start :range-size (- (+ start range-size) next-range-start)} ranges)
+                              [])]
+        (vec (concat last-range-comp empty-space-comp next-range-comp))))))
 
 (def seed-ranges (mapv (fn [[start range-size]] {:start start :range-size range-size}) (partition 2 seeds)))
+
+(def s1 (flatten (map (fn [s] (map-number-range s (first all-ranges))) seed-ranges)))
+(def s2 (flatten (map (fn [s] (map-number-range s (second all-ranges))) s1)))
+(def s3 (flatten (map (fn [s] (map-number-range s (nth all-ranges 2))) s2)))
 
 (def results (reduce (fn [seeds ranges]
                        (flatten (map (fn [s] (map-number-range s ranges)) seeds)))
